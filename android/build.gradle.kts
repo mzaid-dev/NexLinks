8fa8faade96dca5cb24ad52d8563c593472ab62b
Muhammad Zaid <jack@dev.mzaid>
2026-06-11 06:52:27 +0500

    build: fix Gradle evaluation crash and set stable AGP 8.7.3

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -18,20 +18,25 @@ subprojects {
 // Removed evaluationDependsOn which was causing premature project evaluation
 
 subprojects {
-    afterEvaluate {
-        if (project.hasProperty("android")) {
-            val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
-            android.compileOptions {
-                sourceCompatibility = JavaVersion.VERSION_11
-                targetCompatibility = JavaVersion.VERSION_11
-            }
+    plugins.withId("com.android.application") {
+        val android = extensions.getByName("android") as com.android.build.gradle.BaseExtension
+        android.compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
         }
-        
-        // Force Kotlin tasks to use JVM 11
-        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-            kotlinOptions {
-                jvmTarget = "11"
-            }
+    }
+    plugins.withId("com.android.library") {
+        val android = extensions.getByName("android") as com.android.build.gradle.BaseExtension
+        android.compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+        }
+    }
+
+    // Force Kotlin tasks to use JVM 11 lazily
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "11"
         }
     }
 }
